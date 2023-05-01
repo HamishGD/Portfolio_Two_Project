a9e27684553c08e85b86f55dade10135b33b13ed
hamish <hamishgeorgedavie@gmail.com>
2023-05-01 20:07:57 +1000

    This is going to have to do

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -38,7 +38,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
-        The Exercises That You <br /> Just Have To Know
+        The Workouts That You Must <br /> Know!
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
